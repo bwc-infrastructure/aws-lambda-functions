ed4b616a27734a11b30391c72706df86e87295b4
NaveenKerati <accounts@bridgeworks.cloud>
2023-04-10 22:20:14 +0530

    initial commit

---
+++ .github/workflows/create-s3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+
+# Check if the bucket exists
+if aws s3 ls "s3://${{BUCKET_NAME}}" 2>&1 | grep -q 'NoSuchBucket'; then
+    echo "Bucket does not exist. Creating new bucket..."
+    # Create the bucket with default options
+    aws s3 mb "s3://${{BUCKET_NAME}}"
+else
+    echo "Bucket already exists"
+fi
