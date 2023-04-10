0688ad81401d5618164a1329f829a3420ddf52e8
NaveenKerati <accounts@bridgeworks.cloud>
2023-04-10 22:36:54 +0530

    Update create-s3.sh

--- create-s3.sh
+++ create-s3.sh
@@ -3,10 +3,10 @@
 
 
 # Check if the bucket exists
-if aws s3 ls "s3://${{BUCKET_NAME}}" 2>&1 | grep -q 'NoSuchBucket'; then
+if aws s3 ls "s3://${BUCKET_NAME}" 2>&1 | grep -q 'NoSuchBucket'; then
     echo "Bucket does not exist. Creating new bucket..."
     # Create the bucket with default options
-    aws s3 mb "s3://${{BUCKET_NAME}}"
+    aws s3 mb "s3://${BUCKET_NAME}"
 else
     echo "Bucket already exists"
 fi
